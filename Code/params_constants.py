80463559e94811dd695558bdaaad56688544b3a8
feng-ecohydro <41303280+feng-ecohydro@users.noreply.github.com>
2018-07-17 18:52:55 -0500

    Create params_constants.py

---
+++ Code/params_constants.py
@@ -0,0 +1,13 @@
+
+v = 1.557           # conversion from mol/s to m3/day for liquid water
+a = 1.6             # ratio of diffusivity water vapor/CO2
+P_baro = 90.00      # kPa, atmospheric pressure at 1000 m 
+
+nu = 10**6          # unit conversion Pa/MPa  
+rhoH2O = 1000       # density of water, kg/m3
+g = 9.81            # acceleration due to gravity, m/s2
+Mw = 0.018          # molecular weight of water, kg/mol
+rhoCO2 = 1.98       # density of CO2, kg/m3
+Mco2 = 0.044        # molecular weight of CO2, kg/mol, ...so 45mol/m3
+Sd = 36000          # day lengths in seconds, assume 10 hrs of usable daylight
+CO2conc = 0.000400  # in mol/mol
